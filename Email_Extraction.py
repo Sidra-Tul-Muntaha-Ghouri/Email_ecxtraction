6b8889ea0758cc72d106be2a1f48981f47dc3d76
Sidra Tul Muntaha <sidratulmuntaha135@gmail.com>
2023-10-06 11:50:53 +0500

    Update Email_Extraction.py

--- Email_Extraction.py
+++ Email_Extraction.py
@@ -89,47 +89,6 @@ if add and pas:
                 key='download-csv'
                 )
                 
-
-            # flnme = st.text_input('Enter Excel file name (e.g. email_data.xlsx)')
-            # if flnme != "":
-            #     if flnme.endswith(".xlsx") == False:  # add file extension if it is forgotten
-            #         flnme = flnme + ".xlsx"
-
-            #     buffer = BytesIO()
-            #     with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
-            #         df.to_excel(writer, sheet_name='Report')
-
-            #     st.download_button(label="Download Excel workbook", data=buffer.getvalue(), file_name=flnme, mime="application/vnd.ms-excel")
-            # else:
-            #     st.write('Click download button to download as Excel Sheet')
-            #     download_df = st.button('Download')
-            #     if download_df:
-            #         file_name = st.text_input('Enter a file name for the Excel file (e.g., email_data.xlsx)')
-            #         if file_name:
-            #             file_path = f"{file_name}.xlsx"
-            #             df.to_excel(file_path, index=False)
-            #             st.success(f"File '{file_path}' has been downloaded successfully.")
-
-
-            # if df is not None:
-            #     file_name = st.text_input('Enter a file name for the Excel file (e.g., email_data.xlsx)')
-            #     download = st.download_button(label='Download Excel', data=df.to_excel(index=False, header=True), key='download')
-
-            #     if file_name and download:
-            #         with open(file_name, "wb") as f:
-            #             f.write(download)
-            #         st.success(f"File '{file_name}' has been downloaded successfully.")
-
-
-            # if df is not None:
-            #     file_name = st.text_input('Enter a file name for the Excel file (e.g., email_data.xlsx)')
-    
-            #     if st.button('Download Excel') and file_name:
-            #         with pd.ExcelWriter(file_name, engine='xlsxwriter') as writer:
-            #             df.to_excel(writer, sheet_name='Sheet1', index=False, header=True)
-            #         st.success(f"File '{file_name}' has been downloaded successfully.")
-
-
          
     except imaplib.IMAP4.error as e:
         st.error('Authentication failed. Please check your email address and password.')
@@ -139,34 +98,3 @@ else:
     st.warning('Please enter your email address and password.')
 
 
-
-#mail.login('sidratulmuntaha135@gmail.com', 'bfsh faer jpkz diox')
-
-# Select the mailbox and search for emails
-
-# mail.select('inbox')
-# result, data = mail.search(None, 'SINCE "01-SEP-2023"')
-# 
-     #Parse the email messages and extract information
-# 
-# email_list = []
-# for i in data[0].split():
-    # result, data = mail.fetch(i, '(RFC822)')
-    # raw_email = data[0][1]
-    # email_message = email.message_from_bytes(raw_email)
-    # email_subject = email_message['Subject']
-    # email_sender = email_message['From']
-    # email_date = email_message['Date']
-    # email_list.append([email_subject, email_sender, email_date])
-# 
-     #Create a data frame and export to Excel
-# 
-# df = pd.DataFrame(email_list, columns=['Subject', 'Sender', 'Date'])
-# df.to_excel('email_data.xlsx', index=False)
-# 
-       #Disconnect from the IMAP server
-# 
-# mail.close()
-# mail.logout()
-# 
-# df.head()
